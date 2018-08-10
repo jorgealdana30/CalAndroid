70649b52d86331e6f8eee05ec0136564e76d3388
Jorge Aldana <jorgeelieceraldana.92@gmail.com>
2018-08-09 20:12:46 -0500

    Original

--- app/src/main/java/com/dreft/calculadora/Principal.java
+++ app/src/main/java/com/dreft/calculadora/Principal.java
@@ -8,12 +8,6 @@ public class Principal extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-
-
-
-
-        
         setContentView(R.layout.activity_principal);
     }
 }
